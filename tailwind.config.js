a03141f111a418c1a3ad99f0e4ddc40e4abc550c
Fabian Osorio Iriarte <fabiani@uninorte.edu.co>
2020-10-05 20:31:16 -0500

    tailwind initial configuration and customization

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      color: {
+        primary: {
+          100: '#B49FCC',
+          200: '#6D466B',
+          300: '#412234',
+        },
+        secondary: '#EAD7D7',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
