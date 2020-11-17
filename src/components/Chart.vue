c01b81e69adf1d4eba39a3234dea461094a02565
Fabian Osorio <fabian.osorio.990628@gmail.com>
2020-11-16 19:49:45 -0500

    Added format to data in model

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,65 +1,75 @@
 <script>
-import { Line } from 'vue-chartjs'
+import { Line } from "vue-chartjs";
 
 export default {
   extends: Line,
   // components: { Bar },
   data: () => ({
     chartdata: {
-      labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-      datasets: [{
-        label: 'Cubic interpolation (monotone)',
-        data: [0, 20, 20, 60, 60, 120, 130, 180, 120, 125, 105, 110, 170],
-        borderColor: '#36a2eb',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        fill: false,
-        cubicInterpolationMode: 'monotone'
-      }, {
-        label: 'Cubic interpolation (default)',
-        data: [0, 20, 20, 60, 60, 120, 130, 180, 120, 125, 105, 110, 170],
-        borderColor: '#36a2eb',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        fill: false,
-      }, {
-        label: 'Linear interpolation',
-        data: [0, 20, 20, 60, 60, 120, 130, 180, 120, 125, 105, 110, 170],
-        borderColor: '#4bc0c0',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        fill: false,
-        tension: 0
-      }]
+      labels: [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+      ],
+      datasets: [
+        {
+          label: "Cubic interpolation (monotone)",
+          data: [0, 20, 20, 60, 60, 120, 130],
+          borderColor: "#4bc0c0",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          fill: false,
+          cubicInterpolationMode: "monotone",
+        },
+        {
+          label: "Cubic interpolation (default)",
+          data: [0, 20, 20, 60, 60, 120, 130, 180, 120, 125, 105, 110, 170],
+          borderColor: "#36a2eb",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          fill: false,
+        },
+      ],
     },
     options: {
       responsive: true,
       title: {
         display: true,
-        text: 'Chart.js Line Chart - Cubic interpolation mode'
+        text: "Chart.js Line Chart - Cubic interpolation mode",
       },
       tooltips: {
-        mode: 'index'
+        mode: "index",
       },
       scales: {
         x: {
           display: true,
           scaleLabel: {
-            display: true
-          }
+            display: true,
+          },
         },
         y: {
-          display: true,
+          display: false,
           scaleLabel: {
-            display: true,
-            labelString: 'Value'
+            display: false,
+            labelString: "Value",
           },
           suggestedMin: -10,
-          suggestedMax: 200
-        }
-      }
-    }
+          suggestedMax: 200,
+        },
+      },
+    },
   }),
 
   async mounted() {
-    this.renderChart(this.chartdata, this.options)
-  }
-}
+    this.renderChart(this.chartdata, this.options);
+  },
+};
 </script>
--- src/views/Model.vue
+++ src/views/Model.vue
@@ -5,28 +5,51 @@
 </template>
 
 <script>
-import Chart from '@/components/Chart.vue'
+import Chart from "@/components/Chart.vue";
 
 export default {
-  name: 'Model',
+  name: "Model",
   components: {
-    Chart
+    Chart,
   },
   data: function () {
     return {
-      classManager: 'hidden',
-      sidebarSelected: 'px-4 flex justify-end border-r-4 cursor-pointer border-primary-100',
-      sidebarNoSelected: 'px-4 flex justify-end border-r-4 cursor-pointer border-white',
-    }
+      classManager: "hidden",
+      sidebarSelected:
+        "px-4 flex justify-end border-r-4 cursor-pointer border-primary-100",
+      sidebarNoSelected:
+        "px-4 flex justify-end border-r-4 cursor-pointer border-white",
+      prediction: {
+        F: [
+          {
+            end_index: 2553.0,
+            peak_index: 2403,
+            start_index: 2253.0,
+          },
+          {
+            end_index: 3437.0,
+            peak_index: 3287,
+            start_index: 3137.0,
+          },
+        ],
+        N: [],
+        S: [],
+        V: [],
+      },
+      signals: {
+        original: [],
+        preprocessed: [],
+      },
+    };
   },
   methods: {
     addClass() {
-      if (this.classManager === '') {
-        this.classManager = 'hidden';
+      if (this.classManager === "") {
+        this.classManager = "hidden";
       } else {
-        this.classManager = '';
+        this.classManager = "";
       }
-    }
+    },
   },
-}
+};
 </script>
